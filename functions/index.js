5265c43809a93d23884104f1cbd8cdf5c708a99c
jose.garcia.atencia <jose.garcia.atencia@gmail.com>
2025-07-02 03:51:55 +0000

    Integrado con Brevo

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,18 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at
- * https://firebase.google.com/docs/functions
- */
+const {onRequest} = require("firebase-functions/v2/https");
+const {defineSecret} = require("firebase-functions/params");
 
-const functions = require("firebase-functions");
-
-// const {setGlobalOptions} = require("firebase-functions");
-// const {onRequest} = require("firebase-functions/https");
-// Removed unused 'logger' import
-// const logger = require("firebase-functions/logger");
+// Definir el secret de Brevo
+const brevoApiKey = defineSecret("BREVO_API_KEY");
 
 // Import your Express application from server.js
-// Make sure your server.js file exports your Express app like:
-// module.exports = app;
 const app = require("./server");
 
-
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container,
-// so this will be the maximum concurrent request count.
-// setGlobalOptions({ maxInstances: 10 }); // Applying options per function
-// is recommended for v2, but global options can be set here if using v1
-// http triggers.
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
 /**
- * Expone tu aplicación Express como una función HTTP de Firebase.
- * La ruta base será /api cuando despliegues.
- * Tus rutas de Express (ej. /brevo-proxy/register-contact) serán
- * relativas a /api.
+ * Expone tu aplicación Express como una función HTTP de Firebase v2.
+ * Incluye el secret de Brevo para que esté disponible en el runtime.
  */
-exports.api = functions.https.onRequest(app);
+exports.api = onRequest({
+  cors: true,
+  maxInstances: 10,
+  secrets: [brevoApiKey] // Hace que el secret esté disponible
+}, app);
